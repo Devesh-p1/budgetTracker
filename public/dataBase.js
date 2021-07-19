e6d5057172aa73a5d423c7f9c52337f6b96e3cca
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-19 17:30:47 -0400

    added more to db

--- public/dataBase.js
+++ public/dataBase.js
@@ -6,3 +6,10 @@ const indexedDB =
   window.shimIndexedDB;
 
 let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
