1d3a5cb4497642133077fd0e2238be7ba2f24ea8
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-19 17:59:42 -0400

    added event listener to worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -10,3 +10,12 @@ var urlsToCache = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
   ];
+
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
